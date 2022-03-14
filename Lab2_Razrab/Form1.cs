872f4e2647a84d7eb91b03eec9d3bb5063c44a51
Ivan-Maslov <ivanmax98@yandex.ru>
2022-03-14 19:01:15 +0300

    Lab-2

---
+++ Lab2_Razrab/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab2_Razrab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a = Double.Parse(textBox1.Text);
+            double b = Double.Parse(textBox2.Text);
+            textBox3.Text = Convert.ToString(a + b);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double a = Double.Parse(textBox1.Text);
+            double b = Double.Parse(textBox2.Text);
+            textBox3.Text = Convert.ToString(a - b);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double a = Double.Parse(textBox1.Text);
+            double b = Double.Parse(textBox2.Text);
+            textBox3.Text = Convert.ToString(a * b);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            double a = Double.Parse(textBox1.Text);
+            double b = Double.Parse(textBox2.Text);
+            textBox3.Text = Convert.ToString(a / b);
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char num = e.KeyChar;
+            if (!Char.IsDigit(num))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            Char num = e.KeyChar;
+            if (!Char.IsDigit(num))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ Lab2_Razrab/Lab2_Razrab/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Lab2_Razrab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab2_Razrab")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1e7325ba-0d35-4b41-b6ca-b09e204e4a7b")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
